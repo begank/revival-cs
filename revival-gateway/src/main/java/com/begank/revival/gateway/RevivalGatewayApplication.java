5c8049fd88b1e799f1e443d65a250624a751ffe0
jeremylbq <1037395014@qq.com>
2021-12-27 22:27:52 +0800

    [gw]添加网关模块及基础路由配置

---
+++ revival-gateway/src/main/java/com/begank/revival/gateway/RevivalGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.begank.revival.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevivalGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevivalGatewayApplication.class, args);
+    }
+
+}
